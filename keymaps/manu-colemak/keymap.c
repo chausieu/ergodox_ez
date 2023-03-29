0be7b10ccf561f94731540b2df35160455f3d5d2
chausieu <chausieu@gmail.com>
2023-03-29 02:51:03 -0700

    add encoder to right side

--- keymaps/manu-colemak/keymap.c
+++ keymaps/manu-colemak/keymap.c
@@ -631,12 +631,13 @@ void led_set_user(uint8_t usb_led) {
   }
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (clockwise) {
     tap_code(KC_UP);
   } else {
     tap_code(KC_DOWN);
   }
+  return false;
 }
 
 uint32_t layer_state_set_user(uint32_t state)
